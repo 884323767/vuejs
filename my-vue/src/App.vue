08678a90cfa9c40a7899acfca3884e764812d2f0
lewei.li <lewei.li@reorientgroup.com>
2016-08-09 21:30:52 +0800

    Add vuejs

---
+++ my-vue/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+   <button id='show-modal' @click='shwoModal = true'>{{msg}}</button>
+   <modal :show.sync="shwoModal">
+   <h3 slot='header'>custom header</h3>
+  </div>
+</template>
+
+<script>
+import modal from './components/modal'
+
+export default {
+  components: {
+    modal
+  },
+  data (){
+    return{
+      shwoModal:false,
+      msg: 'click',
+    }
+  }
+}
+</script>
+
+<style scope>
+  #show-modal{
+    margin:10%;
+    padding: 10px;
+  }
+</style>
